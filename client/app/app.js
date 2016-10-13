73e8af7ca67fc0f7149b2b75b2093dd97b2c3f2f
coke <jorge.cuadrado.contractor@i4s.com>
2016-10-13 18:07:37 +0200

    Created menu toolbar and offers view

--- client/app/app.js
+++ client/app/app.js
@@ -4,12 +4,12 @@
 angular.module('myApp', [
   'ngRoute',
   'ngMaterial',
-  'myApp.view1',
+  'myApp.offers',
   'myApp.view2',
   'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/offers'});
 }]);
---
+++ client/app/offers/offers_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.offers module', function() {
+
+  beforeEach(module('myApp.offers'));
+
+  describe('offers controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var offersCtrl = $controller('OffersCtrl');
+      expect(offersCtrl).toBeDefined();
+    }));
+
+  });
+});
