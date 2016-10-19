cd2cb079f8c26bc9bad1877b6945d1c651679cd1
Jorge Cuadrado Saez <jorge.cuadrado.contractor@i4s.com>
2016-10-19 18:58:04 +0200

    search bar controler

--- client/src/app/offers/offers.component.js
+++ client/src/app/offers/offers.component.js
@@ -3,9 +3,27 @@
 function offersCtrl (Offers) {
 	var vm = this;
 
+	vm.search = ""
+	vm.category = ""
+
 	vm.$onInit = function () {
 		Offers.get().then(function (offers) { vm.offers = offers; })
 	};
+
+	$scope.search = function() {
+		var query = ""
+		if (vm.search !== "") {
+			query = "search="+vm.search
+		}
+		if (vm.category !== "") {
+			if ( query !== "") {
+				query = query+"&category="+vm.category
+			} else {
+				query = "category="+vm.category
+			}
+		}
+    	Offers.search(query).then(function (offers) { vm.offers = offers; })
+  	};
 
 }
 
