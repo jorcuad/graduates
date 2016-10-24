a016430b4f6c5ded721700d1a8fbb01e9ce5a811
Jorge Cuadrado Saez <jorge.cuadrado.contractor@i4s.com>
2016-10-24 12:58:34 +0200

    check if login form is empty in controller to avoid post and regex input check

--- client/src/app/login/login.component.js
+++ client/src/app/login/login.component.js
@@ -8,12 +8,14 @@ function loginCtrl (LoginService) {
 	};
 
 	vm.do_login = function () {
-		LoginService.login({"username":vm.nick, "password":vm.password})
-					.then( function(answer) {
-						//TODO, sacar toast de todo ok.
-					}, function() {
-						//TODO, sacar toast de ha habido un error.
-					});
+		if ( check_login ( vm.nick, vm.password ) ) {
+			LoginService.login({"username":vm.nick, "password":vm.password})
+						.then( function(answer) {
+							//TODO, sacar toast de todo ok.
+						}, function() {
+							//TODO, sacar toast de ha habido un error.
+						});
+		}
 	}
 }
 
@@ -21,3 +23,16 @@ angular.module('graduatesApp').component('login', {
 	templateUrl: 'app/login/login.html',
 	controller: loginCtrl
 });
+
+function check_login ( nick, pass ) {
+	var nick_regex = new RegExp('^(?=.{5,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?![_.])$');
+	var pass_regex = new RegExp('^(?=.{8,20}$)[a-zA-Z0-9@*#]+$')
+	var nick_valid = nick_regex.exec(nick);
+	var pass_valid = pass_regex.exec(pass);
+	
+	if ( nick_valid && pass_valid && pass !== undefined && nick !== undefined) {
+		return true;
+	} else {
+		return false;
+	}
+}
