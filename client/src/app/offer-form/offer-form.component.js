54eccee7e86ad7fa277f3cca9a9a50bae4c475c3
Jorge Cuadrado Saez <jorge.cuadrado.contractor@i4s.com>
2016-10-24 17:33:16 +0200

    controller and client data check

--- client/src/app/offer-form/offer-form.component.js
+++ client/src/app/offer-form/offer-form.component.js
@@ -3,16 +3,34 @@
 function offerFormCtrl ($http, OfferForm) {
 	var vm = this;
 
-	vm.search = ""
-	vm.categories = ""
-	vm.category = ""
-
 	vm.$onInit = function () {
 		vm.minDate = new Date();
-		vm.username = "Manuel";
-		vm.offerform = {}
+		vm.form = {"user_id":"","active":"", "private":"","activity_date":"", 
+					"offer_name":"", "description":"", "place":"", "category":""}
+		vm.form.user_id = 1;
+		vm.form.active = true;
+		vm.form.private = false;
+		vm.activity_hour = ""
+		vm.activity_min = ""
+		vm.search = ""
+		vm.categories = ""
 		OfferForm.getCategories().then(function (categories) { vm.categories = categories; })
 	};
+
+	vm.create = function(){
+		if(check_form(vm.form) && check_time(vm.activity_hour, vm.activity_min)) {
+
+			vm.form.activity_date = add_time(vm.form.activity_date, vm.activity_hour-1, vm.activity_min)
+
+			OfferForm.create(vm.form).then(function (answer) { 
+				//TODO Mensaje de all green en TOAST de inicio y redireccionar a pagina inicio
+			}, function(answer) {
+				//TODO mensaje de error en TOAST
+			});
+		} else {
+			//TODO mostrar toast de error
+		}
+	}
 }
 
 angular.module('graduatesApp').component('offerForm', {
@@ -20,42 +38,27 @@ angular.module('graduatesApp').component('offerForm', {
 	controller: offerFormCtrl
 });
 
-/*
-	function pad(n) {
-		return (n < 10) ? ("0" + n) : n;
+function check_form(form) {
+	var is_ok = true
+	for(var field in form) {
+ 		if( String(form[field]) == "" || String(form[field]) === undefined) {
+ 			is_ok = false
+ 		}
 	}
+	return is_ok
+}
 
-	function correctName($scope){
-		if($scope.offerform.name==""){
-			return false;
-			}
-		else {
-			return true;
-		}
+function check_time(hour, min) {
+	var is_ok = true
+	if( String(hour) == "" || String(hour) === undefined || String(min) == "" || String(min) === undefined) {
+ 		return false
 	}
-	function correctPlace($scope){
-		if($scope.offerform.place==""){
-			return false;
-			}
-		else {
-			return true;
-		}
-	}
-	function correctCategories($scope){
-		if($scope.offerform.categories==""){
-			return false;
-			}
-		else {
-			return true;
-		}
-	}
-	function correctDate($scope){
-		var today =  new Date();
+	return true
+}
 
-		if($scope.offerform.activity_date > today) {
-			return true;
-		} else {
-			return false;
-		}
-	};
-*/
+function add_time(date, hour, min) { //FIXME
+	var hour_sec = 3600000
+	var min_sec = 60000
+	date.setHours(date.getHours()+hour)
+	return new Date(date.getTime() + (min * min_sec))
+}
