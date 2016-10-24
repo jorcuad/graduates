7bbe16ccbd90d840bdd6c502d6df6958bf08d8b3
pablomuri <pablomuri@gmail.com>
2016-10-24 12:14:47 +0200

    actualizado ofertas con defaults y active=True

--- server/ofertas/models.py
+++ server/ofertas/models.py
@@ -4,14 +4,14 @@ from django.contrib.auth.models import User
 class Offer(models.Model):
     # TODO: buscar problema null
 
-	offer_name = models.CharField(max_length=200, null=True)
-	description = models.CharField(max_length=1000,null=True)
-	pub_date = models.DateTimeField(auto_now_add=True, auto_now=False, null = True)
-	activity_date = models.DateTimeField('activity_date', null=True)
-	categories = models.CharField(max_length=500,null=True)
-	place = models.CharField(max_length=100,null=True)
-	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-	modified_date =  models.DateTimeField(auto_now_add=False, auto_now=True, null = True)
+	offer_name = models.CharField(max_length=200, default="sin título")
+	description = models.CharField(max_length=1000, default="sin descricción")
+	pub_date = models.DateTimeField(auto_now_add=True, auto_now=False)
+	activity_date = models.DateTimeField('activity_date')
+	categories = models.CharField(max_length=500, default="otros")
+	place = models.CharField(max_length=100, default="sin lugar")
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	modified_date =  models.DateTimeField(auto_now_add=False, auto_now=True)
 	active = models.BooleanField(default=True)
 	private = models.BooleanField(default=False)
 
