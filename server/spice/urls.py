c00d6a056c5709d0193f02e021312acfecbc2f86
Pablo Murillo <pablomuri@gmail.com>
2016-11-02 11:47:24 +0100

    Eliminada una linea que causaba conflictos al loguearse

--- server/spice/urls.py
+++ server/spice/urls.py
@@ -32,7 +32,6 @@ router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', admin.site.urls),
     url(r'^', include(router.urls)),
     url(r'^login/', obtain_jwt_token),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
